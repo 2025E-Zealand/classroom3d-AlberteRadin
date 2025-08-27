f7b45e7cd1e9cdaa7efb49463e831f65ad596ef4
AlberteRadin <alberte@radin.dk>
2025-08-27 11:50:13 +0200

    Lavet student class

---
+++ ClassRoomNet60/NewFolder/Student.cs
@@ -0,0 +1,18 @@
+﻿public class Student
+{
+    string Name { get; set; }
+    int BirthMonth { get; set; }
+    int BirthDay { get; set; }
+
+    public Student(string name, int birthMonth, int birthDay)
+    {
+        Name = name;
+        BirthMonth = birthMonth;
+        BirthDay = birthDay;
+    }
+
+
+
+
+
+}

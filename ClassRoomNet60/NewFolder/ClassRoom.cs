b69c1b5bf593629ebfddc74f10f3e66a56c79c33
AlberteRadin <alberte@radin.dk>
2025-08-27 12:27:39 +0200

    Lavet ClassRoom klasse

---
+++ ClassRoomNet60/NewFolder/ClassRoom.cs
@@ -0,0 +1,14 @@
+﻿public class ClassRoom
+{
+    string ClassName { get; set; }
+    List<Student> students;
+    DateTime SemesterStart;
+
+    public ClassRoom(string className , DateTime semesterStart)
+    {
+        ClassName = className;
+        students = new List<Student>();
+        SemesterStart = semesterStart;
+            
+    }
+}
